2bf369b57875ab6b0034fcb6c720d7d0bae2d6ff
Muxammad <swe.muhammad@gmail.com>
2023-04-18 11:13:49 +0500

    LastUpdated

--- src/EmailService.Web/Services/EmailService.cs
+++ src/EmailService.Web/Services/EmailService.cs
@@ -6,7 +6,6 @@ using MailKit.Net.Smtp;
 using MailKit.Security;
 
 namespace EmailService.Web.Services;
-
 public class EmailService : IEmailService
 {
     private readonly IConfiguration _configuration;
@@ -16,12 +15,14 @@ public class EmailService : IEmailService
     }
     public async Task SendASync(EmailMessage emailMessage)
     {
+        //
         var email = new MimeMessage();
         email.From.Add(MailboxAddress.Parse(this._configuration["EmailAddress"]));
         email.To.Add(MailboxAddress.Parse(emailMessage.To));
         email.Subject = emailMessage.Subject;
         email.Body = new TextPart(TextFormat.Html) { Text = emailMessage.Body };
-
+        
+        //
         var smpt = new SmtpClient();
         await smpt.ConnectAsync(this._configuration["Host"], 587, SecureSocketOptions.StartTls);
         await smpt.AuthenticateAsync(this._configuration["EmailAddress"], this._configuration["Password"]);
