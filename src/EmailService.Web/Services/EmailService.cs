98a50c24524dd5b7ee7f81c567d245c7804e3c1e
Muxammad <swe.muhammad@gmail.com>
2023-04-18 11:06:07 +0500

    Email Serivce Done to use

--- src/EmailService.Web/Services/EmailService.cs
+++ src/EmailService.Web/Services/EmailService.cs
@@ -1,12 +1,31 @@
 ﻿using EmailService.Web.Interfaces;
 using EmailService.Web.Models;
+using MimeKit;
+using MimeKit.Text;
+using MailKit.Net.Smtp;
+using MailKit.Security;
 
 namespace EmailService.Web.Services;
 
 public class EmailService : IEmailService
 {
-    public Task SendASync(EmailMessage emailMessage)
+    private readonly IConfiguration _configuration;
+    public EmailService(IConfiguration configuration)
     {
-        throw new NotImplementedException();
+        this._configuration = configuration.GetSection("Email");   
+    }
+    public async Task SendASync(EmailMessage emailMessage)
+    {
+        var email = new MimeMessage();
+        email.From.Add(MailboxAddress.Parse(this._configuration["EmailAddress"]));
+        email.To.Add(MailboxAddress.Parse(emailMessage.To));
+        email.Subject = emailMessage.Subject;
+        email.Body = new TextPart(TextFormat.Html) { Text = emailMessage.Body };
+
+        var smpt = new SmtpClient();
+        await smpt.ConnectAsync(this._configuration["Host"], 587, SecureSocketOptions.StartTls);
+        await smpt.AuthenticateAsync(this._configuration["EmailAddress"], this._configuration["Password"]);
+        await smpt.SendAsync(email);
+        await smpt.DisconnectAsync(true);
     }
 }
